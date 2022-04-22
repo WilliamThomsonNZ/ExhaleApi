7da871692632bf1ef8817ff4c4a41542f52fa00d
WilliamThomsonNZ <william__thomson@outlook.com>
2022-04-22 14:26:09 +1200

    Implement end point

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 // Add Express
 const express = require("express");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+require("dotenv").config({ path: ".env" });
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: "us20",
+});
+
+const mailChimpSignUp = async (email) => {
+  const response = await mailchimp.lists.addListMember(
+    process.env.MAILCHIMP_LIST_ID,
+    {
+      email_address: email,
+      status: "pending",
+    }
+  );
+  return response;
+};
 
 // Initialize Express
 const app = express();
 
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+//End point to add email to list
+app.post("/api/email", async (req, res) => {
+  const userEmail = req.query.email;
+  if (userEmail === "")
+    res.status(400).send("error when trying to sign up new customer");
+  try {
+    const response = await mailChimpSignUp(userEmail);
+    if (response.statusCode == 400) {
+      res.status(400).send("error when trying to sign up new customer");
+    } else {
+      res.status(200).send("Email signed up");
+    }
+  } catch (err) {
+    res.status(400).send("error when trying to sign up new customer");
+  }
 });
 
 // Initialize server
-app.listen(5000, () => {
+app.listen(6000, () => {
   console.log("Running on port 5000.");
 });
 
