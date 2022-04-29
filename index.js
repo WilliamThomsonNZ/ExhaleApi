9946ffc5ed84e2616e67b30bad3604fdbc6e07c2
WilliamThomsonNZ <william__thomson@outlook.com>
2022-04-29 17:03:22 +1200

    Implement endpoint to handle contact form submissions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Add Express
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 const mailchimp = require("@mailchimp/mailchimp_marketing");
+const bodyParser = require("body-parser");
 require("dotenv").config({ path: ".env" });
 
 mailchimp.setConfig({
@@ -29,7 +31,6 @@ app.use(
 );
 //End point to add email to list
 app.post("/api/email", async (req, res) => {
-  console.log("requesteREceieved");
   const userEmail = req.query.email;
   if (userEmail === "")
     res.json({
@@ -50,10 +51,47 @@ app.post("/api/email", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "API RUNNING" });
+  res.json({ message: process });
 });
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: process.env.NODE_MAILER_EMAIL,
+    pass: process.env.NODE.NODE_MAILER_PASSWORD,
+  },
+});
+
+const jsonParser = bodyParser.json();
+app.post("/api/contact", jsonParser, (req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+  const message = req.body.message;
+
+  const options = {
+    from: proceess.env.NODE_MAILER_EMAIL,
+    to: process.env.NODE_MAILER_PASSWORD,
+    subject: "New form submission from Exhale Yoga contact page",
+    text: `
+      NAME: ${name}
+      EMAIL: ${email}
+      MESSAGE: ${message}
+    `,
+  };
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+      res.json({ code: 400, message: err });
+      return;
+    }
+    res.json({ code: 200, message: info.response });
+  });
+});
+
 // Initialize server
-app.listen(6000, () => {});
+app.listen(6000, () => {
+  console.log("Hot on 6000");
+});
 
 // Export the Express API
 module.exports = app;
